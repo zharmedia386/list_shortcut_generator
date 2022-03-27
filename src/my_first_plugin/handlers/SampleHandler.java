f23a1cdbdbab8974dca6a9940367210269f946ce
M Azhar Alauddin <80077273+zharmedia386@users.noreply.github.com>
2022-03-27 23:54:11 +0700

    List Shortcut Generator

---
+++ src/my_first_plugin/handlers/SampleHandler.java
@@ -0,0 +1,23 @@
+package my_first_plugin.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class SampleHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"List Shortcut Generator",
+				"MongoDB Connection to certain connection \n"
+				+ "Class with certain constructor and methods \n"
+						+ "Getter and Setter methods");
+		return null;
+	}
+}
